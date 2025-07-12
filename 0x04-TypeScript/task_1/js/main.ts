aa8aa0f67652fc55e38eac58c02355d0bd19fddb
prince joseph udoewah <princepiper28@gmail.com>
2025-07-12 14:57:11 +0200

    Add Directors interface extending Teacher

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,4 +1,4 @@
- interface Teacher {
+interface Teacher {
   readonly firstName: string;
   readonly lastName: string;
   fullTimeEmployee: boolean;
@@ -7,6 +7,7 @@
   [key: string]: any; // allows additional properties like "contract"
 }
 
+// Example of a Teacher object with an extra property "contract"
 const teacher3: Teacher = {
   firstName: 'John',
   fullTimeEmployee: false,
@@ -17,3 +18,18 @@ const teacher3: Teacher = {
 
 console.log(teacher3);
 
+// Directors interface that extends Teacher and adds numberOfReports
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+// Example of a Directors object
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
